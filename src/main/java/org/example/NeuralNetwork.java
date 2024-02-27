4860835e0f0d8839b059469efe283d5538340c82
AndreyZavarukhin <a.zavarukhin2004@gmail.com>
2024-02-27 07:33:11 +0400

    first

---
+++ src/main/java/org/example/NeuralNetwork.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class NeuralNetwork extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println(update.getMessage().getFrom());
+    }
+
+
+}
